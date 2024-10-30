b4bfb316845e61e6d87012d5e81281d16ba89a92
antonalley <anton.alley@gmail.com>
2024-10-30 11:52:25 -0600

    nothing

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,10 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="text-xl bold">
           Truck Stop Fitness  
-        </div>        
+        </div> 
+        <div>
+          Details Coming Soon
+        </div>       
       </main>
     </div>
   );
